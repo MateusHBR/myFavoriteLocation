3f4567361e669664dfba48c46aeaac680aa77103
MateusHBR <mateusmorais.r@gmail.com>
2020-08-19 12:26:00 -0300

    importando projeto ao git

---
+++ android/app/src/main/kotlin/br/com/mateusramalho/my_favorite_location/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.mateusramalho.my_favorite_location
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:my_favorite_location/app/app_module.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+void main() => runApp(ModularApp(module: AppModule()));
