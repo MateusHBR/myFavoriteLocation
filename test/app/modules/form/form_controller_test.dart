06ecae6ec552c8b02e447853330ac3dec9c8d958
MateusHBR <mateusmorais.r@gmail.com>
2020-08-20 11:04:32 -0300

    arquivos de teste

---
+++ test/app/modules/form/form_controller_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_modular/flutter_modular_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:my_favorite_location/app/modules/form/form_controller.dart';
+import 'package:my_favorite_location/app/modules/form/form_module.dart';
+
+void main() {
+  initModule(FormModule());
+  // FormController form;
+  //
+  setUp(() {
+    //     form = FormModule.to.get<FormController>();
+  });
+
+  group('FormController Test', () {
+    //   test("First Test", () {
+    //     expect(form, isInstanceOf<FormController>());
+    //   });
+
+    //   test("Set Value", () {
+    //     expect(form.value, equals(0));
+    //     form.increment();
+    //     expect(form.value, equals(1));
+    //   });
+  });
+}
