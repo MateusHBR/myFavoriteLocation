1af929bafb42906e41e193ba814f1921249d63a4
MateusHBR <mateusmorais.r@gmail.com>
2020-08-20 13:56:21 -0300

    store de lugares

---
+++ lib/app/shared/stores/store_grate_places.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'store_grate_places.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
+mixin _$StoreGratePlaces on _StoreGratePlacesBase, Store {
+  Computed<ObservableList<Place>> _$itemsComputed;
+
+  @override
+  ObservableList<Place> get items =>
+      (_$itemsComputed ??= Computed<ObservableList<Place>>(() => super.items,
+              name: '_StoreGratePlacesBase.items'))
+          .value;
+
+  @override
+  String toString() {
+    return '''
+items: ${items}
+    ''';
+  }
+}
