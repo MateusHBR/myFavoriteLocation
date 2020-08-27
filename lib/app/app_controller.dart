4e85b1e60062af0e4cd3051fcab76c087957ee4a
MateusHBR <mateusmorais.r@gmail.com>
2020-08-27 20:26:11 -0300

    checando geo location status

--- lib/app/app_controller.dart
+++ lib/app/app_controller.dart
@@ -1,3 +1,4 @@
+import 'package:geolocator/geolocator.dart';
 import 'package:mobx/mobx.dart';
 
 part 'app_controller.g.dart';
@@ -5,11 +6,9 @@ part 'app_controller.g.dart';
 class AppController = _AppControllerBase with _$AppController;
 
 abstract class _AppControllerBase with Store {
-  @observable
-  int value = 0;
-
-  @action
-  void increment() {
-    value++;
+  _AppControllerBase() {
+    geolocator.checkGeolocationPermissionStatus();
   }
+
+  final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
 }
