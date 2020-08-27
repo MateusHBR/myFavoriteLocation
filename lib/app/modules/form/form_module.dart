67b0498fb59ce3979b3398637c37ae72c2246fd7
MateusHBR <mateusmorais.r@gmail.com>
2020-08-27 20:31:26 -0300

    adicionando rota para a tela de mapa

--- lib/app/modules/form/form_module.dart
+++ lib/app/modules/form/form_module.dart
@@ -1,3 +1,6 @@
+import 'package:my_favorite_location/app/modules/form/screens/map_screen.dart';
+import 'package:my_favorite_location/app/shared/stores/store_greate_places.dart';
+
 import 'form_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:dio/dio.dart';
@@ -12,6 +15,14 @@ class FormModule extends ChildModule {
   @override
   List<Router> get routers => [
         Router(Modular.initialRoute, child: (_, args) => FormPage()),
+        Router(
+          '/map_screen',
+          child: (_, args) => MapScreen(
+            currentLocation: args.data[0],
+            isReadOnly: args.data[1],
+          ),
+          transition: TransitionType.scale,
+        ),
       ];
 
   static Inject get to => Inject<FormModule>.of();
