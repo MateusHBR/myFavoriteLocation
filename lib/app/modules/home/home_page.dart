b92ea631f68ff872b140f0db3f3e54a6432bf090
MateusHBR <mateusmorais.r@gmail.com>
2020-08-27 20:33:29 -0300

    home page

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mobx/mobx.dart';
 import 'home_controller.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,10 +17,14 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: appBar(),
       body: Column(
-        children: <Widget>[],
+        children: <Widget>[
+          listOfItems(size),
+        ],
       ),
       floatingActionButton: floatingButton(),
     );
@@ -38,6 +44,62 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
           },
         ),
       ],
+    );
+  }
+
+  Widget listOfItems(Size size) {
+    return Expanded(
+      child: Observer(
+        builder: (_) {
+          if (controller.itemFuture.error != null ||
+              controller.itemFuture.status == FutureStatus.rejected) {
+            return Center(
+              child: Text('Erro Ao Carregar Dados!'),
+            );
+          }
+
+          if (controller.itemFuture.status == FutureStatus.pending) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (controller.items.length == 0) {
+            return Center(
+              child: Text('Nenhum Item Cadastrado!'),
+            );
+          }
+
+          return ListView.separated(
+            itemCount: controller.items.length,
+            separatorBuilder: (context, index) => Divider(),
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: ListTile(
+                  leading: controller.items[index].image == null
+                      ? CircleAvatar(
+                          backgroundColor: Colors.purple,
+                        )
+                      : CircleAvatar(
+                          backgroundImage: FileImage(
+                            controller.items[index].image,
+                          ),
+                        ),
+                  title: Text('${controller.items[index].title}'),
+                  subtitle: Text('${controller.items[index].location.address}'),
+                  onTap: () {
+                    Modular.link.pushNamed(
+                      'detail-screen',
+                      arguments: controller.items[index],
+                    );
+                  },
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 
