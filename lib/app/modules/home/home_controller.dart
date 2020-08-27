17bafe7537dc9e691aa869a6d89697feef35df7d
MateusHBR <mateusmorais.r@gmail.com>
2020-08-27 20:32:37 -0300

    lista de iens

--- lib/app/modules/home/home_controller.dart
+++ lib/app/modules/home/home_controller.dart
@@ -1,15 +1,21 @@
 import 'package:mobx/mobx.dart';
+import 'package:my_favorite_location/app/shared/models/place.dart';
+import 'package:my_favorite_location/app/shared/stores/store_greate_places.dart';
 
 part 'home_controller.g.dart';
 
 class HomeController = _HomeControllerBase with _$HomeController;
 
 abstract class _HomeControllerBase with Store {
-  @observable
-  int value = 0;
+  final StoreGreatePlaces _storeGratePlaces;
 
-  @action
-  void increment() {
-    value++;
-  }
+  _HomeControllerBase(this._storeGratePlaces);
+
+  @computed
+  ObservableList<Place> get items =>
+      [..._storeGratePlaces.items].asObservable();
+
+  @computed
+  ObservableFuture<List<Map<String, dynamic>>> get itemFuture =>
+      _storeGratePlaces.getItemFuture.asObservable();
 }
