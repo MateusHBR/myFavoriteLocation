6b14b342b464dc942e244e805daec8240b4e6a2f
MateusHBR <mateusmorais.r@gmail.com>
2020-08-27 20:35:16 -0300

    métodos fromMap e ToMap

--- lib/app/shared/models/place.dart
+++ lib/app/shared/models/place.dart
@@ -7,7 +7,7 @@ class PlaceLocation {
   final double longitude;
   final String address;
 
-  PlaceLocation({
+  const PlaceLocation({
     @required this.latitude,
     @required this.longitude,
     this.address,
@@ -26,4 +26,21 @@ class Place {
     @required this.location,
     @required this.image,
   });
+
+  Place.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    title = json['title'];
+    image = File(json['image']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'image': image.path.toString(),
+      'latitude': location.latitude,
+      'longitude': location.longitude,
+      'address': location.address,
+    };
+  }
 }
