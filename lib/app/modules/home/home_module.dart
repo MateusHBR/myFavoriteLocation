b5f00a3e72d9246d23dce92e45bbba19c24ee33d
MateusHBR <mateusmorais.r@gmail.com>
2020-08-27 20:33:21 -0300

    adicionando rota de detalhes

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,3 +1,6 @@
+import 'package:my_favorite_location/app/modules/home/screens/place_detail_screen.dart';
+import 'package:my_favorite_location/app/shared/stores/store_greate_places.dart';
+
 import 'home_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:dio/dio.dart';
@@ -12,6 +15,12 @@ class HomeModule extends ChildModule {
   @override
   List<Router> get routers => [
         Router(Modular.initialRoute, child: (_, args) => HomePage()),
+        Router(
+          'detail-screen',
+          child: (_, args) => PlaceDetailScreen(
+            place: args.data,
+          ),
+        ),
       ];
 
   static Inject get to => Inject<HomeModule>.of();
